e99d047f73514dc5dc125656d7fe61c2dc8045e6
Ashir N Basheer <ashirnbasheer@gmail.com>
2022-02-15 17:32:51 +0530

    commiting the data

---
+++ src/data.js
@@ -0,0 +1,81 @@
+const foodItems = [
+
+
+        {
+            id:1,
+            title:'veg dish',
+            image:'https://www.acouplecooks.com/wp-content/uploads/2021/04/Breakfast-Skillet-017.jpg',
+            desc:'This breakfast skillet recipe is a hearty way to start the day! Cook up cheesy eggs and potatoes in ',
+            price:100,
+            category:'breakfast'
+        },
+        {
+            id:2,
+            title:'Pancake',
+            image:'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/american-style-pancakes-34d56dc.jpg?quality=90&resize=440,400',
+            desc:'This breakfast skillet recipe is a hearty way to start the day! Cook up cheesy eggs and potatoes in ',
+            price:200,
+            category:'breakfast'
+        },
+        {
+            id:3,
+            title:'Waffle',
+            image:'https://d3d127vhjgkwcw.cloudfront.net/images/articles/2019_07/_best-breakfast-recipes-_waffle_breakfast_sandwich_with_chorizo_.jpg',
+            desc:'This breakfast skillet recipe is a hearty way to start the day! Cook up cheesy eggs and potatoes in ',
+            price:150,
+            category:'breakfast'
+        },
+        {
+            id:4,
+            title:'Noodles',
+            image:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/garlic-spaghetti-horizontal-1539203011.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*',
+            desc:'This breakfast skillet recipe is a hearty way to start the day! Cook up cheesy eggs and potatoes in ',
+            price:300,
+            category:'Dinner'
+        },
+        {
+            id:5,
+            title:'Fried rice',
+            image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9Ysepht59jgKNMoYUvJZEM7xGFPkAoUKOvA&usqp=CAU',
+            desc:'This breakfast skillet recipe is a hearty way to start the day! Cook up cheesy eggs and potatoes in ',
+            price:200,
+            category:'Dinner'
+        },
+        {
+            id:6,
+            title:'Samosa Chat',
+            image:'https://static.toiimg.com/photo/59217136.cms',
+            desc:'This breakfast skillet recipe is a hearty way to start the day! Cook up cheesy eggs and potatoes in ',
+            price:50,
+            category:"snacks"
+
+        },
+        {
+            id:7,
+            title:'French Fries',
+            image:'https://www.thespruceeats.com/thmb/IHKuXcx3uUI1IWkM_cnnQdFH-zQ=/3485x2323/filters:fill(auto,1)/how-to-make-homemade-french-fries-2215971-hero-01-02f62a016f3e4aa4b41d0c27539885c3.jpg',
+            desc:'Chocolate. Vanilla. Strawberry. When it comes to milk shakes, everyone has a favorite flavor, and we ',
+            price:100,
+            category:"snacks"
+        },
+        {
+            id:8,
+            title:'Mocha Milk Shake',
+            image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo5wylRXIMs-DWrh44GSGKcjS1VFT1tE6wcw&usqp=CAU',
+            desc:'Chocolate. Vanilla. Strawberry. When it comes to milk shakes, everyone has a favorite flavor, and we ',
+            price:100,
+            category:"shakes"
+        },
+        {
+            id:9,
+            title:'Oreo-Shake',
+            image:'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2010%2F03%2F30%2Fcookies-and-cream-milk-shakes-ay-4x3-1-2000.jpg&q=60',
+            desc:'Blend Oreos, ice cream, and milk for an indulgent summer milk shake',
+            price:100,
+            category:"shakes"
+        },
+
+
+]
+
+module.exports = foodItems;
